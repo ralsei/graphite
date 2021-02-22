6c8490d0515c7604861152754e3c57539a73808a
hazel levine <hazel@knightsofthelambdacalcul.us>
2021-02-22 14:58:04 -0500

    yeah

--- gapminder.rkt
+++ gapminder.rkt
@@ -11,8 +11,8 @@
          #:x-label "GDP per capita (USD)"
          #:y-label "Life expectancy (years)"
          #:mapping (aes #:x "gdpPercap" #:y "lifeExp" #:discrete-color "continent")
-         #:x-transform log-transform
-         ;#:x-conv (log _ 10)
-         ;#:x-ticks (ticks-scale (linear-ticks) (invertible-function (expt _ 10) (log _ 10)))
+         ;#:x-transform log-transform
+         #:x-conv (log _ 10)
+         #:x-ticks (ticks-scale (plot-x-ticks) (invertible-function (expt 10 _) (log _ 10)))
          (ppoints)
          (fit #:method 'power #:mapping (aes #:width 3))))
