f4c0da38c33f52521a429c3144d3b2d0a56fb764
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2021-04-12 14:57:43 -0400

    fix deps

--- graphite-examples/info.rkt
+++ graphite-examples/info.rkt
@@ -1,4 +1,13 @@
 #lang info
 
 (define collection "graphite-examples")
-(define deps '("graphite"))
+(define deps '("base"
+               "data-frame"
+               "fancy-app"
+               "gregor-lib"
+               "http-easy"
+               "pict-lib"
+               "plot-gui-lib"
+               "plot-lib"
+               "threading-lib"
+               "graphite"))
