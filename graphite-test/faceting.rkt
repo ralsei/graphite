6534eb7321c68efdd0d2ea368de5b55d5d05906e
hazel levine <hazel@knightsofthelambdacalcul.us>
2021-05-23 18:28:26 -0400

    two more facet tests -- pausing to test something

--- graphite-test/faceting.rkt
+++ graphite-test/faceting.rkt
@@ -10,7 +10,7 @@
     (let ([int-data (make-data-frame)]
           [xs (build-vector 1000 (λ (_) (* (random) 30)))]
           [ys (build-vector 1000 (λ (_) (* (random) 30)))]
-          [strats (for/vector ([n (in-range 0 1000)]
+          [strats (for/vector ([_ (in-range 0 1000)]
                                [x (in-cycle (in-list '("a" "b" "c" "d" "e")))])
                     x)])
       (df-add-series! int-data (make-series "x-var" #:data xs))
@@ -20,8 +20,23 @@
 (define facet-1
   (graph #:data facet-1-df
          #:mapping (aes #:x "x-var" #:y "y-var" #:facet "stratify-on")
-         #:title "Default faceting behavior"
          (points)))
 
+(define-runtime-path facet-2-data "./test-data/facet-2.dat")
+(define facet-2
+  (graph #:data facet-1-df
+         #:mapping (aes #:x "x-var" #:y "y-var" #:facet "stratify-on")
+         #:facet-wrap 5
+         (lines)))
+
+(define-runtime-path facet-3-data "./test-data/facet-3.dat")
+(define facet-3
+  (graph #:data facet-1-df
+         #:mapping (aes #:x "x-var" #:facet "stratify-on")
+         #:facet-wrap 1
+         (density)))
+
 (module+ test
-  (check-draw-steps facet-1 facet-1-data))
+  (check-draw-steps facet-1 facet-1-data)
+  (check-draw-steps facet-2 facet-2-data)
+  (check-draw-steps facet-3 facet-3-data))
