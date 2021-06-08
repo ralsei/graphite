2e353be9d3c7e727b6f28597e0f6d64a7c42ee63
hazel levine <hazel@knightsofthelambdacalcul.us>
2021-06-08 19:47:02 -0400

    clean up duplicated/unnecessary/overengineered examples

--- graphite-examples/facet-wrap.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-(require data-frame graphite)
-
-(define gss (df-read/csv "data/gss_sm.csv"))
-
-(graph #:data gss
-       #:mapping (aes #:x "religion" #:facet "bigregion")
-       #:width 700 #:height 700
-       (bar #:mode 'prop))
--- graphite-examples/organs.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-(require data-frame fancy-app graphite plot/utils)
-
-(define organdata (df-read/csv "./data/organdata.csv" #:na "NA"))
-
-(graph #:data organdata
-       #:mapping (aes #:x "country" #:y "donors")
-       (boxplot))
